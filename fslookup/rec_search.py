58be98a15edde7c305537faf33a4f163479a73f1
Marko Klei <markoklei@gmail.com>
2020-08-25 15:29:24 +0300

    added support for .docx and started testing .txt functionality

--- fslookup/rec_search.py
+++ fslookup/rec_search.py
@@ -1,26 +1,41 @@
 from os import walk
 from os.path import isfile, isdir, join, splitext
 
+import docx
 
-SUPPORTED_EXTENSIONS = ['.txt']
+
+SUPPORTED_EXTENSIONS = ['.txt', '.docx']
 
 
-"""function assumes <path> points to a .txt file and searches it for <sterm>.
+"""assumes <path> points to a .txt file and searches it for <sterm>.
 Returns True if file contains <sterm>, false otherwise."""
 def open_search_txt(path, sterm):
-    pass
+    with open(path) as reader:
+        if sterm in reader.read():
+            return True
+    return False
 
 
-SEARCH_FUNCTIONS = [open_search_txt]
+"""assumes <path> points to a .docx file and searches it for <sterm>.
+ Returns True if file contains <sterm>, false otherwise."""
+def open_search_docx(path, sterm):
+    doc = docx.Document(path)
+    for para in doc.paragraphs:
+        if sterm in para.text:
+            return True
+    return False
 
 
-"""function assumes path is pointing to a file, checks file's extension and tries to open it if extension is supported.
+SEARCH_FUNCTIONS = [open_search_txt, open_search_docx]
+
+
+"""assumes path is pointing to a file, checks file's extension and tries to open it if extension is supported.
 Returns True if file contains <sterm>, false otherwise. If file extension is not supported, return False."""
 def contains (path, sterm):
     fname, fextension = splitext(path)
-    for i, ext in enumerate(SUPPORTED_EXTENSIONS):
-        if ext == fextension:
-            return SEARCH_FUNCTIONS[i](path, sterm)
+    if fextension in SUPPORTED_EXTENSIONS:
+        return SEARCH_FUNCTIONS[SUPPORTED_EXTENSIONS.index(fextension)](path, sterm)
+    return False
 
 
 
@@ -35,6 +50,13 @@ def rec_search (curr_path, sterm, found):
     # in the sense that there is no such file/directory, then we  would get false on the if clause and think it is a directory
     if isdir(curr_path):
         _, subdirs, fs = next(walk(curr_path))
-        for obj in fs.extend(subdirs):
+        fs.extend(subdirs)  # subdirs can't be None, so no need for 'subdirs or []'
+        for obj in fs:  # fs now contains the names of files and folders in the current directory
             rec_search(join(curr_path, obj), sterm, found)
 
+
+found = []
+TESTING_PATH = r"C:\Users\Mark\Desktop\Testing fslookup"
+STERM = "testing"
+rec_search(TESTING_PATH, STERM, found)
+print("called rec_search, found is : ", found)
