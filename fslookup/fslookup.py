9c0f11490a5f729a00ceb80a036c5c1122d70d7e
Marko Klei <markoklei@gmail.com>
2020-10-07 00:50:04 +0300

    added browse button

--- fslookup/fslookup.py
+++ fslookup/fslookup.py
@@ -1,4 +1,6 @@
+from tkinter import filedialog  # this is a submodule that isn't loaded automatically
 import tkinter as tk
+import os
 
 # Create the GUI application main windows
 top = tk.Tk()
@@ -17,8 +19,24 @@ sterm_fld.insert(0, 'Enter your search term ...')   # Add default text
 sterm_fld.pack()
 
 
-#   - Add a path field
+#   - Add a path selection button
+def browse_dialog():
+    filename = tk.filedialog.askopenfilename(initialdir = '/',
+                                             title = 'Select file/folder to begin search...',
+                                             filetypes = ( ('Text files - .txt', '*.txt'),
+                                                           ('Word files - .docx', '*.docx'),
+                                                           ('All files', '*.*') ))
 
+    st = 'Chose the following ' + ('folder:\t' if os.path.isdir(filename) else 'file:\t') + filename
+    browse_label.configure(text = st)
+
+browse_label = tk.Label(top,
+                        text = 'Choose file/folder to begin search...')
+path_fld = tk.Button(top,
+                     text = 'Browse',
+                     command = browse_dialog)
+browse_label.pack()
+path_fld.pack()
 
 #   - Add an execution button
 
